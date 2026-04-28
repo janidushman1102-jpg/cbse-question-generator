b44dad6604e1c374a357815134405155fd84cf47
janidushman1102-jpg <janidushman1102@gmail.com>
2026-04-28 14:13:20 +0530

    Implement Google Sign-In and authentication methods

--- utils/authService.js
+++ utils/authService.js
@@ -1,1 +1,33 @@
 // authService.js content goes here
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { GoogleAuthProvider, signInWithCredential, signOut, onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../config/firebaseConfig';
+
+GoogleSignin.configure({
+  webClientId: '524538800032-i90tfemniomjijvgbqpecdvk2gekq20r.apps.googleusercontent.com',
+});
+
+export const signInWithGoogle = async () => {
+  await GoogleSignin.hasPlayServices();
+  const userInfo = await GoogleSignin.signIn();
+
+  const idToken = userInfo.idToken;
+  const credential = GoogleAuthProvider.credential(idToken);
+
+  const result = await signInWithCredential(auth, credential);
+  return result.user;
+};
+
+export const signOutUser = async () => {
+  await GoogleSignin.signOut();
+  await signOut(auth);
+};
+
+export const getCurrentUser = () => {
+  return new Promise((resolve) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      unsubscribe();
+      resolve(user);
+    });
+  });
+};
