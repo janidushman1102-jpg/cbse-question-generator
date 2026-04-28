c345e363f843bb3b3068d1bf17074b7c4917940c
janidushman1102-jpg <janidushman1102@gmail.com>
2026-04-28 13:20:26 +0530

    Update Firebase configuration with new project details

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "your-sender-id",
-  appId: "your-app-id"
+  apiKey: "AIzaSyCCtiBSoT3J2I5WyZpZF040q7TqvbuiGFY",
+  authDomain: "cbse-question-generator.firebaseapp.com",
+  projectId: "cbse-question-generator",
+  storageBucket: "cbse-question-generator.firebasestorage.app",
+  messagingSenderId: "685043625043",
+  appId: "1:685043625043:web:47fa2c21defd1507fe2aa6"
 };
 
 const app = initializeApp(firebaseConfig);
